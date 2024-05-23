5ff6755ac4c13963902acafeac9ca8f4e6e54322
Ibrahim <ibrv.cvrtxxn@gmail.com>
2024-05-23 10:55:25 +0200

    updated page information

--- app/account/page.js
+++ app/account/page.js
@@ -10,7 +10,26 @@ async function AccountPage() {
 
   return (
     <div>
-      <p className='text-accent-500'>Welcome, {user?.name}!</p>
+      <p className='text-accent-500 text-[2rem]'>Welcome, {user?.name}!</p>
+      <p className='mt-[2rem] leading-[2rem]'>
+        Discover the perfect retreat in our charming cabins, nestled in
+        nature&apos;s embrace. Our user-friendly reservation page allows you to
+        easily book your next getaway. Whether you&apos;re seeking a peaceful
+        solo escape, a romantic weekend, or a fun-filled family adventure, we
+        have the perfect cabin for you. Start planning your dream vacation with
+        us today!
+      </p>
+
+      <p className='text-accent-500 text-[1.2rem] mt-[2rem]'>
+        Manage Your Profile and Reservations
+      </p>
+      <p className='mt-[1rem] leading-[2rem]'>
+        Stay in control of your vacation plans with our comprehensive profile
+        and reservation management page. Here, you can enjoy the convenience of
+        managing your vacation details all in one place, making your stay with{' '}
+        <span className='text-accent-500'>The Wild Oasis</span> even more
+        delightful and hassle-free.
+      </p>
     </div>
   );
 }
